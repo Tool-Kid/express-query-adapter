bc7aa5f33fad34f06eb4ab34b45d37d578682bbc
Raúl Julián López Caña <rjlopezmad@gmail.com>
2021-09-24 19:50:18 +0200

    refactor: replace `LookupFilter` switch with Map

---
+++ src/field-filter-map.ts
@@ -0,0 +1,78 @@
+import { LookupFilter } from "./lookup.enum";
+import { Like, IsNull, LessThan, LessThanOrEqual, MoreThan, MoreThanOrEqual, In, Between, Not } from 'typeorm';
+
+interface BuildQueryFunction {
+  build: (prop: string, value: string) => any;
+}
+
+export const LOOKUP_FILTER_MAP: Map<LookupFilter, BuildQueryFunction> = new Map([
+  [
+    LookupFilter.EXACT,
+    {
+      build: (prop, value) => ({ [prop]: value }),
+    }
+  ],
+  [
+    LookupFilter.CONTAINS,
+    {
+      build: (prop, value) => ({ [prop]: Like(`%${value}%`) }),
+    }
+  ],
+  [
+    LookupFilter.STARTS_WITH,
+    {
+      build: (prop, value) => ({ [prop]: Like(`${value}%`) }),
+    }
+  ],
+  [
+    LookupFilter.ENDS_WITH,
+    {
+      build: (prop, value) => ({ [prop]: Like(`%${value}`) }),
+    }
+  ],
+  [
+    LookupFilter.IS_NULL,
+    {
+      build: (prop, value) => ({ [prop]: IsNull() }),
+    }
+  ],
+  [
+    LookupFilter.LT,
+    {
+      build: (prop, value) => ({ [prop]: LessThan(value) }),
+    }
+  ],
+  [
+    LookupFilter.LTE,
+    {
+      build: (prop, value) => ({ [prop]: LessThanOrEqual(value) }),
+    }
+  ],
+  [
+    LookupFilter.GT,
+    {
+      build: (prop, value) => ({ [prop]: MoreThan(value) }),
+    }
+  ],
+  [
+    LookupFilter.GTE,
+    {
+      build: (prop, value) => ({ [prop]: MoreThanOrEqual(value) }),
+    }
+  ],
+  [
+    LookupFilter.IN,
+    {
+      build: (prop, value) => ({ [prop]: In(value.split(',')) }),
+    }
+  ],
+  [
+    LookupFilter.BETWEEN,
+    {
+      build: (prop, value) => {
+        const rangeValues = value.split(',');
+        return { [prop]: Between(+rangeValues[0], +rangeValues[1]) };
+      },
+    }
+  ],
+]);
--- src/field-filter.ts
+++ src/field-filter.ts
@@ -1,6 +1,7 @@
+import { LOOKUP_FILTER_MAP } from './field-filter-map';
+import { Not } from 'typeorm';
 import { AbstractFilter } from './filter';
 import { LookupFilter } from './lookup.enum';
-import { Like, IsNull, LessThan, LessThanOrEqual, MoreThan, MoreThanOrEqual, In, Between, Not } from 'typeorm';
 
 export class FieldFilter extends AbstractFilter {
 
@@ -15,49 +16,20 @@ export class FieldFilter extends AbstractFilter {
 
     let queryToAdd;
 
-    switch(this.lookup) {
-      case LookupFilter.EXACT:
-        queryToAdd = { [this.prop]: this.value };
-        break;
-      case LookupFilter.CONTAINS:
-        queryToAdd = { [this.prop]: Like(`%${this.value}%`) };
-        break;
-      case LookupFilter.STARTS_WITH:
-        queryToAdd = { [this.prop]: Like(`${this.value}%`) };
-        break;
-      case LookupFilter.ENDS_WITH:
-        queryToAdd = { [this.prop]: Like(`%${this.value}`) };
-        break;
-      case LookupFilter.IS_NULL:
-        queryToAdd = { [this.prop]: IsNull() };
-        break;
-      case LookupFilter.LT:
-        queryToAdd = { [this.prop]: LessThan(this.value) };
-        break;
-      case LookupFilter.LTE:
-        queryToAdd = { [this.prop]: LessThanOrEqual(this.value) };
-        break;
-      case LookupFilter.GT:
-        queryToAdd = { [this.prop]: MoreThan(this.value) };
-        break;
-      case LookupFilter.GTE:
-        queryToAdd = { [this.prop]: MoreThanOrEqual(this.value) };
-        break;
-      case LookupFilter.IN:
-        queryToAdd = { [this.prop]: In(this.value.split(',')) };
-        break;
-      case LookupFilter.BETWEEN:
-        const rangeValues = this.value.split(',');
-        queryToAdd = { [this.prop]: Between(+rangeValues[0], +rangeValues[1]) };
-        break;
-    }
+    queryToAdd = this.setQuery(queryToAdd);
+
     if(this.notOperator) {
       queryToAdd[this.prop] = Not(queryToAdd[this.prop]);
     }
+
     this.query['where'] = {
       ...this.query['where'],
       ...queryToAdd
     }
   }
-}
 
+  private setQuery(queryToAdd: any) {
+    queryToAdd = LOOKUP_FILTER_MAP.get(this.lookup).build(this.prop, this.value);
+    return queryToAdd;
+  }
+}
