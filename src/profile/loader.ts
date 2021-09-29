a9145b222d41c91513b9518e4deefc3b77a48a42
Raúl Julián López Caña <rjlopezmad@gmail.com>
2021-09-29 22:09:46 +0200

    feat: add profile `loader`

---
+++ src/profile/loader.ts
@@ -0,0 +1,15 @@
+import { ConfigProfile } from "./config-profile";
+import { DISABLED_PROFILE, ENABLED_PROFILE } from "./defaults";
+
+export const loadProfile = (profile: 'enabled' | 'disabled' | ConfigProfile): ConfigProfile => {
+  if (!profile) {
+    return DISABLED_PROFILE;
+  }
+  if (profile === 'enabled') {
+    return ENABLED_PROFILE;
+  } else if (profile === 'disabled') {
+    return DISABLED_PROFILE;
+  } else {
+    return profile;
+  }
+}
