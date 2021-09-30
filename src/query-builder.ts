a0d3b68a2bfd8ad16a20cfad39bcae2766640122
Raúl Julián López Caña <rjlopezmad@gmail.com>
2021-09-30 09:35:59 +0200

    feat: load profile on `QueryBuilder`

--- src/query-builder.ts
+++ src/query-builder.ts
@@ -3,18 +3,25 @@ import { FindManyOptions } from 'typeorm';
 import { TypeORMQuery } from './typeorm-query';
 import { FilterFactory } from './filter/filter-factory';
 import { OptionsCollection } from './filter/options/container';
+import { ConfigProfile } from './profile/config-profile';
+import { loadProfile } from './profile/loader';
 
 export class QueryBuilder {
 
   private expressQuery: ExpressQuery;
   private typeORMQuery: TypeORMQuery;
 
+  private readonly profile: ConfigProfile;
   private readonly findOptions: OptionsCollection;
   private readonly filterFactory: FilterFactory;
 
-  constructor(expressQuery: ExpressQuery) {
+  constructor(
+    expressQuery: ExpressQuery,
+    profile?: 'enabled' | 'disabled' | ConfigProfile,
+  ) {
     this.expressQuery = expressQuery;
     this.typeORMQuery = {};
+    this.profile = loadProfile(profile);
     this.findOptions = new OptionsCollection();
     this.filterFactory = new FilterFactory();
   }
@@ -22,10 +29,13 @@ export class QueryBuilder {
   public build(): FindManyOptions {
 
     for (const option of this.findOptions.options) {
-      option.setOption({
-        source: this.expressQuery,
-        target: this.typeORMQuery,
-      });
+      option.setOption(
+        {
+          source: this.expressQuery,
+          target: this.typeORMQuery,
+        },
+        this.profile,
+      );
     }
 
     for (const queryItem in this.expressQuery) {
