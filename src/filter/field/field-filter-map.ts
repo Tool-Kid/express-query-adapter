a3c93299f51ead86bdcd13d9c6f3acfec3adef70
Raúl Julián López Caña <rjlopezmad@gmail.com>
2022-11-11 17:42:44 +0100

    test: testing for `between-dates` filter

--- src/filter/field/field-filter-map.ts
+++ src/filter/field/field-filter-map.ts
@@ -16,94 +16,103 @@ interface BuildQueryFunction {
   build: (prop: string, value: string) => Record<string, FindOptionsUtils>
 }
 
-export const LOOKUP_FILTER_MAP: Map<LookupFilter, BuildQueryFunction> = new Map(
+export const LOOKUP_FILTER_MAP: Map<LookupFilter, BuildQueryFunction> = new Map([
   [
-    [
-      LookupFilter.EXACT,
-      {
-        build: (prop, value) => ({ [prop]: value }),
+    LookupFilter.EXACT,
+    {
+      build: (prop, value) => ({ [prop]: value }),
+    },
+  ],
+  [
+    LookupFilter.CONTAINS,
+    {
+      build: (prop, value) => ({ [prop]: Like(`%${value}%`) }),
+    },
+  ],
+  [
+    LookupFilter.STARTS_WITH,
+    {
+      build: (prop, value) => ({ [prop]: Like(`${value}%`) }),
+    },
+  ],
+  [
+    LookupFilter.ENDS_WITH,
+    {
+      build: (prop, value) => ({ [prop]: Like(`%${value}`) }),
+    },
+  ],
+  [
+    LookupFilter.ICONTAINS,
+    {
+      build: (prop, value) => ({ [prop]: ILike(`%${value}%`) }),
+    },
+  ],
+  [
+    LookupFilter.ISTARTS_WITH,
+    {
+      build: (prop, value) => ({ [prop]: ILike(`${value}%`) }),
+    },
+  ],
+  [
+    LookupFilter.IENDS_WITH,
+    {
+      build: (prop, value) => ({ [prop]: ILike(`%${value}`) }),
+    },
+  ],
+  [
+    LookupFilter.IS_NULL,
+    {
+      build: (prop) => ({ [prop]: IsNull() }),
+    },
+  ],
+  [
+    LookupFilter.LT,
+    {
+      build: (prop, value) => ({ [prop]: LessThan(value) }),
+    },
+  ],
+  [
+    LookupFilter.LTE,
+    {
+      build: (prop, value) => ({ [prop]: LessThanOrEqual(value) }),
+    },
+  ],
+  [
+    LookupFilter.GT,
+    {
+      build: (prop, value) => ({ [prop]: MoreThan(value) }),
+    },
+  ],
+  [
+    LookupFilter.GTE,
+    {
+      build: (prop, value) => ({ [prop]: MoreThanOrEqual(value) }),
+    },
+  ],
+  [
+    LookupFilter.IN,
+    {
+      build: (prop, value) => ({ [prop]: In(value.split(',')) }),
+    },
+  ],
+  [
+    LookupFilter.BETWEEN,
+    {
+      build: (prop, value) => {
+        const rangeValues = value.split(',')
+        return { [prop]: Between(+rangeValues[0], +rangeValues[1]) }
       },
-    ],
-    [
-      LookupFilter.CONTAINS,
-      {
-        build: (prop, value) => ({ [prop]: Like(`%${value}%`) }),
+    },
+  ],
+  [
+    LookupFilter.BETWEEN_DATES,
+    {
+      build: (prop, value) => {
+        const rangeValues = value.split(',')
+        return {
+          [prop]: Between(new Date(rangeValues[0]), new Date(rangeValues[1])),
+        }
       },
-    ],
-    [
-      LookupFilter.STARTS_WITH,
-      {
-        build: (prop, value) => ({ [prop]: Like(`${value}%`) }),
-      },
-    ],
-    [
-      LookupFilter.ENDS_WITH,
-      {
-        build: (prop, value) => ({ [prop]: Like(`%${value}`) }),
-      },
-    ],
-    [
-      LookupFilter.ICONTAINS,
-      {
-        build: (prop, value) => ({ [prop]: ILike(`%${value}%`) }),
-      },
-    ],
-    [
-      LookupFilter.ISTARTS_WITH,
-      {
-        build: (prop, value) => ({ [prop]: ILike(`${value}%`) }),
-      },
-    ],
-    [
-      LookupFilter.IENDS_WITH,
-      {
-        build: (prop, value) => ({ [prop]: ILike(`%${value}`) }),
-      },
-    ],
-    [
-      LookupFilter.IS_NULL,
-      {
-        build: (prop) => ({ [prop]: IsNull() }),
-      },
-    ],
-    [
-      LookupFilter.LT,
-      {
-        build: (prop, value) => ({ [prop]: LessThan(value) }),
-      },
-    ],
-    [
-      LookupFilter.LTE,
-      {
-        build: (prop, value) => ({ [prop]: LessThanOrEqual(value) }),
-      },
-    ],
-    [
-      LookupFilter.GT,
-      {
-        build: (prop, value) => ({ [prop]: MoreThan(value) }),
-      },
-    ],
-    [
-      LookupFilter.GTE,
-      {
-        build: (prop, value) => ({ [prop]: MoreThanOrEqual(value) }),
-      },
-    ],
-    [
-      LookupFilter.IN,
-      {
-        build: (prop, value) => ({ [prop]: In(value.split(',')) }),
-      },
-    ],
-    [
-      LookupFilter.BETWEEN,
-      {
-        build: (prop, value) => {
-          const rangeValues = value.split(',')
-          return { [prop]: Between(+rangeValues[0], +rangeValues[1]) }
-        },
-      },
-    ],
-  ]
-)
+    },
+  ],
+])
