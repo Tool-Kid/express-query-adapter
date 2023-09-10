c9ee3352cadfa92432de6e1c4a6729adfe54ab99
Victor Freixinho <victorfn01@gmail.com>
2023-09-10 14:01:11 -0300

    refactor(eslint): add comments to stop eslint warnings

--- packages/express-query-adapter/src/typeorm/filter/field/lookups/is-null.ts
+++ packages/express-query-adapter/src/typeorm/filter/field/lookups/is-null.ts
@@ -3,6 +3,7 @@ import { LookupBuilder } from '../lookup';
 import { QueryDialect } from '../../../../types';
 
 export class IsNullLookup extends LookupBuilder {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   build(prop: string, value: string): Record<string, FindOptionsUtils> {
     if (this.dialect === QueryDialect.MONGODB) {
       return {
