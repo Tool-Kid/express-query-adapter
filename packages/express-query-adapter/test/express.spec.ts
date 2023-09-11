3fc1c2643722ac43aea550f355b9658d2b661188
Victor Freixinho <victorfn01@gmail.com>
2023-09-11 11:44:11 -0300

    test(express): add tests for express integration with mongodb dialect

--- packages/express-query-adapter/test/express.spec.ts
+++ packages/express-query-adapter/test/express.spec.ts
@@ -4,26 +4,39 @@ import { TypeORMQueryBuilder } from '../src/typeorm/query-builder';
 import { DEFAULT_PAGINATION } from './fixtures/default-pagination';
 import { Like } from 'typeorm';
 import { Server } from 'http';
+import { TypeORMQueryDialect } from '../src/typeorm/query-dialect';
 
-describe('Test Express integration', () => {
+describe('Test Express integration with typeorm adapter', () => {
   let server: Server;
 
   beforeAll((done) => {
     const app = express();
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
-    app.get('/get', (req, res) => {
-      const queryBuilder = new TypeORMQueryBuilder({});
+    app.get('/get/:dialect?', (req, res) => {
+      const config: Record<string, unknown> = {};
+      if (req.params.dialect) {
+        config['dialect'] = req.params.dialect as TypeORMQueryDialect;
+      }
+      const queryBuilder = new TypeORMQueryBuilder(config);
       const built = queryBuilder.build(req.query);
       res.send(built);
     });
-    app.post('/post_urlquery', (req, res) => {
-      const queryBuilder = new TypeORMQueryBuilder({});
+    app.post('/post_urlquery/:dialect?', (req, res) => {
+      const config: Record<string, unknown> = {};
+      if (req.params.dialect) {
+        config['dialect'] = req.params.dialect as TypeORMQueryDialect;
+      }
+      const queryBuilder = new TypeORMQueryBuilder(config);
       const built = queryBuilder.build(req.query);
       res.send(built);
     });
-    app.post('/post_body', (req, res) => {
-      const queryBuilder = new TypeORMQueryBuilder({});
+    app.post('/post_body/:dialect?', (req, res) => {
+      const config: Record<string, unknown> = {};
+      if (req.params.dialect) {
+        config['dialect'] = req.params.dialect as TypeORMQueryDialect;
+      }
+      const queryBuilder = new TypeORMQueryBuilder(config);
       const built = queryBuilder.build(req.body);
       res.send(built);
     });
@@ -36,55 +49,120 @@ describe('Test Express integration', () => {
     server.close();
   });
 
-  it('should return an appropiate query built for GET /get?...', (done) => {
-    request(server)
-      .get('/get?name=rjlopezdev&email__contains=@gmail.com')
-      .expect(200)
-      .end((err, res) => {
-        expect(JSON.parse(res.text)).toEqual({
-          where: {
-            name: 'rjlopezdev',
-            email: Like('%@gmail.com%'),
-          },
-          ...DEFAULT_PAGINATION,
+  describe('tests with no provided dialect', () => {
+    it('should return an appropiate query built for GET /get?...', (done) => {
+      request(server)
+        .get('/get?name=rjlopezdev&email__contains=@gmail.com')
+        .expect(200)
+        .end((err, res) => {
+          expect(JSON.parse(res.text)).toEqual({
+            where: {
+              name: 'rjlopezdev',
+              email: Like('%@gmail.com%'),
+            },
+            ...DEFAULT_PAGINATION,
+          });
+          done();
         });
-        done();
-      });
+    });
+
+    it('should return an appropiate query built for POST /post_urlquery?...', (done) => {
+      request(server)
+        .post('/post_urlquery?name=rjlopezdev&email__contains=@gmail.com')
+        .expect(200)
+        .end((err, res) => {
+          expect(JSON.parse(res.text)).toEqual({
+            where: {
+              name: 'rjlopezdev',
+              email: Like('%@gmail.com%'),
+            },
+            ...DEFAULT_PAGINATION,
+          });
+          done();
+        });
+    });
+
+    it('should return an appropiate query built for POST /post_body, body: {...}', (done) => {
+      request(server)
+        .post('/post_body')
+        .send({
+          name: 'rjlopezdev',
+          email__contains: '@gmail.com',
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(JSON.parse(res.text)).toEqual({
+            where: {
+              name: 'rjlopezdev',
+              email: Like('%@gmail.com%'),
+            },
+            ...DEFAULT_PAGINATION,
+          });
+          done();
+        });
+    });
   });
 
-  it('should return an appropiate query built for POST /post_urlquery?...', (done) => {
-    request(server)
-      .post('/post_urlquery?name=rjlopezdev&email__contains=@gmail.com')
-      .expect(200)
-      .end((err, res) => {
-        expect(JSON.parse(res.text)).toEqual({
-          where: {
-            name: 'rjlopezdev',
-            email: Like('%@gmail.com%'),
-          },
-          ...DEFAULT_PAGINATION,
+  describe('tests with "mongodb" dialect', () => {
+    it('should return an appropiate query built for GET /get?...', (done) => {
+      request(server)
+        .get('/get/mongodb?name=rjlopezdev&age__not__gte=50')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).toEqual({
+            where: {
+              $or: [
+                { name: { $eq: 'rjlopezdev' } },
+                { name: { $eq: 'rjlopezdev' } },
+              ],
+              age: { $not: { $gte: 50 } },
+            },
+            ...DEFAULT_PAGINATION,
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('should return an appropiate query built for POST /post_body, body: {...}', (done) => {
-    request(server)
-      .post('/post_body')
-      .send({
-        name: 'rjlopezdev',
-        email__contains: '@gmail.com',
-      })
-      .expect(200)
-      .end((err, res) => {
-        expect(JSON.parse(res.text)).toEqual({
-          where: {
-            name: 'rjlopezdev',
-            email: Like('%@gmail.com%'),
-          },
-          ...DEFAULT_PAGINATION,
+    it('should return an appropiate query built for POST /post_urlquery?...', (done) => {
+      request(server)
+        .post('/post_urlquery/mongodb?name=rjlopezdev&age__not__gte=50')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).toEqual({
+            where: {
+              $or: [
+                { name: { $eq: 'rjlopezdev' } },
+                { name: { $eq: 'rjlopezdev' } },
+              ],
+              age: { $not: { $gte: 50 } },
+            },
+            ...DEFAULT_PAGINATION,
+          });
+          done();
         });
-        done();
-      });
+    });
+
+    it('should return an appropiate query built for POST /post_body, body: {...}', (done) => {
+      request(server)
+        .post('/post_body/mongodb')
+        .send({
+          name: 'rjlopezdev',
+          age__not__gte: '50',
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body).toEqual({
+            where: {
+              $or: [
+                { name: { $eq: 'rjlopezdev' } },
+                { name: { $eq: 'rjlopezdev' } },
+              ],
+              age: { $not: { $gte: 50 } },
+            },
+            ...DEFAULT_PAGINATION,
+          });
+          done();
+        });
+    });
   });
 });
